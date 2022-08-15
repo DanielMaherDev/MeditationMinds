405cdcac842580f492322f908ad61ed092562579
DanielMaherDev <dmdanielmaher@gmail.com>
2022-08-15 21:31:09 +0000

    add sub-menu to self meditate pae

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,15 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
         hamburger.classList.remove("active")
         navMenu.classList.remove("active")
 }))
+
+/* scrolling for anchor links */
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
