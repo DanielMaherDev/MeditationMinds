ce546845ff52a63857b552ff4490b1bab80d9ef2
DanielMaherDev <dmdanielmaher@gmail.com>
2022-08-15 22:32:06 +0000

    re-added js to scroll to anchor links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,24 @@
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
+
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
+
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
         hamburger.classList.remove("active")
         navMenu.classList.remove("active")
 }))
+
+/* scrolling for anchor links */
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
