42726177f832e04b5b4795282c3afbe96649f9f3
DanielMaherDev <dmdanielmaher@gmail.com>
2022-08-18 00:14:25 +0000

    undo defer for youtube iframes

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,12 +22,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-
-/* defer youtube loading */
-function init() {
-    var vidDefer = document.getElementsByTagName('iframe');
-    for (var i=0; i<vidDefer.length; i++) {
-    if(vidDefer[i].getAttribute('data-src')) {
-    vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
-    } } }
-    window.onload = init;
